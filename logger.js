26cf578f4f529e02c35577d2644d70a824c7916a
Billy Moon <billy.moon@ardoq.com>
2021-11-03 23:48:32 +0000

    simplify

--- logger.js
+++
@@ -1,7 +0,0 @@
-const loggerPackage = process.env.SOCK
-  ? "./dgt-logger-socket/client"
-  : process.env.MOCK
-  ? "./dgt-logger-mock/server"
-  : "./dgt-logger";
-
-module.exports = require(loggerPackage)
